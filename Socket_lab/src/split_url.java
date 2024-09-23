469cc74454fe7e76c4fe68baec028ada1ea5bb33
Riya Awal <reeyaawal123@gmail.com>
2024-09-23 07:03:49 +0545

    socket

---
+++ Socket_lab/src/split_url.java
@@ -0,0 +1,27 @@
+import java.net.*;
+public class split_url {
+    public static void main(String[] args) throws Exception {
+       String url="ftp://mp3:mp3@138.247.121.61:21000/c%3a/";
+       try {
+        URL u = new URL(url);
+        System.out.println("The URL is " + u);
+        System.out.println("The scheme is " + u.getProtocol());
+        System.out.println("The user info is " + u.getUserInfo());
+        String host = u.getHost();
+        if (host != null) {
+        int atSign = host.indexOf('@');
+        if (atSign != -1) host = host.substring(atSign+1);
+        System.out.println("The host is " + host);
+        } else {
+        System.out.println("The host is null.");
+        }
+        System.out.println("The port is " + u.getPort());
+        System.out.println("The path is " + u.getPath());
+        System.out.println("The ref is " + u.getRef());
+        System.out.println("The query string is " + u.getQuery());
+        } catch (MalformedURLException ex) {
+        System.err.println(url + " is not a URL I understand.");
+        }
+        System.out.println();
+    }
+}
---
+++ Socket_lab/src/url_connection.java
@@ -0,0 +1,18 @@
+
+import java.net.*;  
+import java.io.*;   
+public class url_connection {
+    public static void main(String[] args) throws Exception {
+      
+        try{    
+            URL url=new URL("http://www.samriddhicollege.com/jbsccsit-college-in-bhaktapur-nepal");    
+            URLConnection urlcon=url.openConnection();    
+            InputStream stream=urlcon.getInputStream();    
+            int i;    
+            while((i=stream.read())!=-1){    
+            System.out.print((char)i);    
+            }    
+            }catch(Exception e){System.out.println(e);}    
+            
+    }
+}
