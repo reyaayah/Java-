19dd7140b5ff63ee3276413fd2368331e4607ca6
Riya Awal <reeyaawal123@gmail.com>
2024-09-25 08:11:01 +0545

    Unit7

---
+++ src/java/My_Servlet.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.*;
+
+public class My_Servlet implements Servlet {
+    ServletConfig config=null;
+
+    @Override
+    public void init(ServletConfig sc) throws ServletException {
+        config =sc;
+        System.out.println("in init");
+       
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter pw=res.getWriter();
+        pw.println("<h1>hello from lifecycle servlet</h1>");
+        System.out.println("in service");
+        
+    }
+    @Override
+    public String getServletInfo(){
+        return "LifeCycleServlet";
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("in destroy");
+    }
+
+}
